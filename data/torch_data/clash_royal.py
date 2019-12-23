d26e7f6e97b47416e07d804e83b20d4a2b792bb6
koibiki <orient2008.cl@hotmail.com>
2019-12-23 21:12:32 +0800

    load data.

--- data/torch_data/clash_royal.py
+++ data/torch_data/clash_royal.py
@@ -26,32 +26,32 @@ class ClayRoyalDataset(data.Dataset):
 			img_path = os.path.join(img_id[0], "running/{}.jpg".format(i))
 			imgs.append(cv2.imread(img_path))
 
-		env_state_path = os.path.join(img_id[0], "env_state.json")
-		with open(env_state_path) as f:
+		env_states_path = os.path.join(img_id[0], "env_states.json")
+		with open(env_states_path) as f:
 			env_states = json.load(f)
 		env_states = env_states[start_index: start_index + 10]
 
-		card_type_path = os.path.join(img_id[0], "card_type.json")
-		with open(card_type_path) as f:
-			card_type = json.load(f)
-		card_type = card_type[start_index: start_index + 10]
+		card_types_path = os.path.join(img_id[0], "card_types.json")
+		with open(card_types_path) as f:
+			card_types = json.load(f)
+		card_types = card_types[start_index: start_index + 10]
 
-		card_property_path = os.path.join(img_id[0], "card_property.json")
-		with open(card_property_path) as f:
-			card_property = json.load(f)
-		card_property = card_property[start_index: start_index + 10]
+		card_properties_path = os.path.join(img_id[0], "card_properties.json")
+		with open(card_properties_path) as f:
+			card_properties = json.load(f)
+		card_properties = card_properties[start_index: start_index + 10]
 
-		action_path = os.path.join(img_id[0], "action.json")
-		with open(action_path) as f:
+		actions_path = os.path.join(img_id[0], "actions.json")
+		with open(actions_path) as f:
 			actions = json.load(f)
 		actions = actions[start_index: start_index + 10]
 
-		reward_path = os.path.join(img_id[0], "reward.json")
+		reward_path = os.path.join(img_id[0], "rewards.json")
 		with open(reward_path) as f:
 			reward = json.load(f)
 		reward = reward[start_index: start_index + 10]
 
-		return imgs, env_states, card_type, card_property, actions, reward
+		return imgs, env_states, card_types, card_properties, actions, reward
 
 	def __len__(self):
 		return len(self.data)
@@ -69,12 +69,10 @@ class ClayRoyalDataset(data.Dataset):
 		data = []
 		for game_id in game_ids:
 			game_id_path = os.path.join(root, game_id)
-			data_dir_names = os.listdir(game_id_path)
-			data_dir_paths = [os.path.join(game_id_path, data_dir_name) for data_dir_name in data_dir_names]
-			for data_dir_path in data_dir_paths:
-				data.append([data_dir_path, len(os.listdir(data_dir_path))])
+			data.append([game_id_path, len(os.listdir(os.path.join(game_id_path, "running")))])
 		return data
 
 
 if __name__ == '__main__':
-	pass
+	root = "F:\\gym_data\\clash_royal\\"
+	ClayRoyalDataset(root)
