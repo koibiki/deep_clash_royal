6f3b228f25f8ee24f3ee3289b1b22a88360caf74
koibiki <orient2008.cl@hotmail.com>
2020-01-01 18:17:36 +0800

    update card dict config.

--- game/parse_result.py
+++ game/parse_result.py
@@ -2,45 +2,12 @@ import numpy as np
 
 CARD_TYPE = 93
 
-card_dict = {0: "empty", 1: "Lightning", 2: "Furnace", 3: "GoblinBarrel", 4: "DarkPrince",
-             5: "Prince", 6: "RoyalHogs", 7: "Freeze", 8: "Giant", 9: "Bowler",
-             10: "Mortar", 11: "Arrows", 12: "FireSpirit", 13: "Bomber", 14: "Bandit",
-             15: "GoblinGiant", 16: "SpearGoblin", 17: "IceGolem", 18: "X-box", 19: "TheLog",
-             20: "BarbarianHut", 21: "Witch", 22: "Knight", 23: "Hunter", 24: "Poison",
-             25: "GoblinHut", 26: "P.E.K.K.A", 27: "BattleRam", 28: "Barbarians", 29: "Tombstone",
-             30: "GiantSnowball", 31: "Executioner", 32: "CannonCart", 33: "Musketeer", 34: "Princess",
-             35: "Archers", 36: "DartGoblin", 37: "InfernoDragon", 38: "Rascals", 39: "MegaKnight",
-             40: "ThreeMusketeer", 41: "RamRider", 42: "IceWizard", 43: "SkeletonBarrel", 44: "MegaMinion",
-             45: "Tesla", 46: "Goblins", 47: "SkeletonArmy", 48: "GoblinGang", 49: "Bats",
-             50: "Lumberjack", 51: "HogRider", 52: "Golem", 53: "CloneSpell", 54: "Fireball",
-             55: "Valkyrie", 56: "Zap", 57: "Guards", 58: "IceSpirit", 59: "NightWitch",
-             60: "Tornado", 61: "BarbarianBarrel", 62: "WallBreakers", 63: "Rage", 64: "miniP.E.K.K.A",
-             65: "Miner", 66: "Wizard", 67: "GiantSkeleton", 68: "Cannon", 69: "Zappies",
-             70: "Graveyard", 71: "BabyDragon", 72: "LavaPups", 73: "Sparky", 74: "FlyingMachine",
-             75: "ElectroDragon", 76: "InfernoTower", 77: "Balloon", 78: "BomberTower", 79: "Minions",
-             80: "MagicArcher", 81: "MinionHorde", 82: "LavaHound", 83: "Rocket", 84: "ElectorWizard",
-             85: "remain0", 86: "remain1", 87: "remain2", 88: "remain3", 89: "remain4",
-             90: "remain5", 91: "remain6", 92: "remain7", 93: "remain8"}
+card_dict = {0: "empty", 1: "Prince", 2: "Bomber", 3: "Arrows", 4: "Witch", 5: "miniP.E.K.K.A", 6: "Zap",
+             7: "BabyDragon", 8: "Musketeer", 9: "Fireball", 10: "SkeletonArmy", 11: "Minions", 12: "Valkyrie"}
 
-elixir_dict = {"empty": 0.0, "Lightning": 0.6, "Furnace": 0.4, "GoblinBarrel": 0.2, "DarkPrince": 0.4,
-               "Prince": 0.5, "RoyalHogs": 0.5, "Freeze": 0.4, "Giant": 0.5, "Bowler": 0.5,
-               "Mortar": 0.4, "Arrows": 0.3, "FireSpirit": 0.2, "Bomber": 0.3, "Bandit": 0.0,
-               "GoblinGiant": 0.6, "SpearGoblin": 0.2, "IceGolem": 0.2, "X-box": 0.6, "TheLog": 0.2,
-               "BarbarianHut": 0.7, "Witch": 0.5, "Knight": 0.3, "Hunter": 0.4, "Poison": 0.4,
-               "GoblinHut": 0.5, "P.E.K.K.A": 0.7, "BattleRam": 0.4, "Barbarians": 0.5, "Tombstone": 0.3,
-               "GiantSnowball": 0.2, "Executioner": 0.0, "CannonCart": 0.5, "Musketeer": 0.4, "Princess": 0.3,
-               "Archers": 0.3, "DartGoblin": 0.3, "InfernoDragon": 0.4, "Rascals": 0.5, "MegaKnight": 0.7,
-               "ThreeMusketeer": 0.9, "RamRider": 0.5, "IceWizard": 0.3, "SkeletonBarrel": 0.3, "MegaMinion": 0.3,
-               "Tesla": 0.4, "Goblins": 0.2, "SkeletonArmy": 0.3, "GoblinGang": 0.3, "Bats": 0.2,
-               "Lumberjack": 0.0, "HogRider": 0.4, "Golem": 0.8, "CloneSpell": 0.0, "Fireball": 0.4,
-               "Valkyrie": 0.4, "Zap": 0.2, "Guards": 0.3, "IceSpirit": 0.1, "NightWitch": 0.0,
-               "Tornado": 0.3, "BarbarianBarrel": 0.2, "WallBreakers": 0.3, "Rage": 0.2, "miniP.E.K.K.A": 0.4,
-               "Miner": 0.3, "Wizard": 0.5, "GiantSkeleton": 0.6, "Cannon": 0.3, "Zappies": 0.0,
-               "Graveyard": 0.5, "BabyDragon": 0.4, "LavaPups": 0.0, "Sparky": 0.0, "FlyingMachine": 0.4,
-               "ElectroDragon": 0.5, "InfernoTower": 0.5, "Balloon": 0.5, "BomberTower": 0.4, "Minions": 0.3,
-               "MagicArcher": 0.0, "MinionHorde": 0.5, "LavaHound": 0.0, "Rocket": 0.6, "ElectorWizard": 0.0,
-               "remain0": 0.0, "remain1": 0.0, "remain2": 0.0, "remain3": 0.0, "remain4": 0.0,
-               "remain5": 0.0, "remain6": 0.0, "remain7": 0.0, "remain8": 0.0}
+elixir_dict = {"empty": 0.0, "Prince": 0.5, "Bomber": 0.3, "Arrows": 0.3, "Witch": 0.5, "miniP.E.K.K.A": 0.4,
+               "Zap": 0.2, "BabyDragon": 0.4, "Musketeer": 0.4, "Fireball": 0.4, "SkeletonArmy": 0.3, "Minions": 0.3,
+               "Valkyrie": 0.4,}
 
 
 def parse_frame_state(result):
@@ -95,11 +62,11 @@ def calu_available_card(card_type, card_property):
         for i, c in enumerate(card_type):
             available_card = [0 for _ in range(94)]
             available_card = np.asarray([available_card])
-            available_card[:,c[0]] = 0 if card_property[i][0] < 0.1 else 1
-            available_card[:,c[1]] = 0 if card_property[i][2] < 0.1 else 1
-            available_card[:,c[2]] = 0 if card_property[i][4] < 0.1 else 1
-            available_card[:,c[3]] = 0 if card_property[i][6] < 0.1 else 1
-            available_card[:,0] = 1
+            available_card[:, c[0]] = 0 if card_property[i][0] < 0.1 else 1
+            available_card[:, c[1]] = 0 if card_property[i][2] < 0.1 else 1
+            available_card[:, c[2]] = 0 if card_property[i][4] < 0.1 else 1
+            available_card[:, c[3]] = 0 if card_property[i][6] < 0.1 else 1
+            available_card[:, 0] = 1
             available_cards.append(available_card)
         available_cards = np.concatenate(available_cards, axis=0)
         return available_cards
