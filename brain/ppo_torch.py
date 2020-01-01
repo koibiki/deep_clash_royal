c316de9c1acc233f445e2b6a2712110920f09a5a
koibiki <orient2008.cl@hotmail.com>
2020-01-01 18:07:58 +0800

    finish train code.

--- brain/ppo_torch.py
+++ brain/ppo_torch.py
@@ -23,9 +23,9 @@ class PPO(object):
     batch_size = 32
     gamma = 0.99
 
-    def __init__(self, ):
+    def __init__(self, device="cpu"):
         super(PPO, self).__init__()
-        self.device = torch.device("cpu")
+        self.device = torch.device(device)
 
         self.ppo_net = PpoNet().to(self.device)
 
@@ -69,19 +69,38 @@ class PPO(object):
             value, critic_hidden = critic
         return value.numpy(), critic_hidden
 
+    def save(self):
+        torch.save(self.ppo_net.state_dict(), './param/params.pth')
+
+    def load(self):
+        self.ppo_net.load_state_dict(torch.load('./param/params.pth', map_location=self.device))
+
     def save_param(self):
-        torch.save(self.ppo_net.state_dict(), '../param/net_param' + str(time.time())[:10] + '.pth')
+        torch.save(self.ppo_net.state_dict(), '../param/' + str(time.time())[:10] + '.pth')
 
     def learn(self, ):
 
         root = "F:\\gym_data\\clash_royal\\"
         dataset = ClayRoyalDataset(root)
 
-        dataloader = DataLoader(dataset=dataset, batch_size=8, shuffle=True)
+        dataloader = DataLoader(dataset=dataset, batch_size=32, shuffle=True)
 
-        for epoch in range(10):
+        total_loss = 0
+        total_value = 0
+        iter = 0
+
+        for epoch in range(1000):
             for i, (imgs, env_states, card_types, card_properties, action_index, action_prob, reward) in enumerate(
                     dataloader):
+
+                imgs = imgs.to(self.device)
+                env_states = env_states.to(self.device)
+                card_types = card_types.to(self.device)
+                card_properties = card_properties.to(self.device)
+                action_index = action_index.to(self.device)
+                action_prob = action_prob.to(self.device)
+                reward = reward.to(self.device)
+
                 actor_hidden = self.ppo_net.init_hidden(len(imgs), self.device)
                 critic_hidden = self.ppo_net.init_hidden(len(imgs), self.device)
                 step_count = random.randint(5, 10)
@@ -99,21 +118,21 @@ class PPO(object):
 
                 advantage = gt_reward - critic
 
-                # advantage = (advantage - torch.mean(advantage)) / (torch.std(advantage) + 1e-8)
+                advantage = (advantage - torch.mean(advantage)) / (torch.std(advantage) + 1e-8)
 
-                new_card_prob = torch.from_numpy(np.array(action['card_prob']))
+                new_card_prob = torch.from_numpy(np.array(action['card_prob'])).to(self.device)
                 old_card_log_prob = action_prob[:, step_count - 1, 0]
                 ratio = torch.div(new_card_prob, old_card_log_prob).view((-1, 1))
                 card_surr1 = ratio * advantage
                 card_surr2 = torch.clamp(ratio, 1 - self.clip_param, 1 + self.clip_param) * advantage
 
-                new_pos_x_prob = torch.from_numpy(np.array(action['pos_x_prob']))
+                new_pos_x_prob = torch.from_numpy(np.array(action['pos_x_prob'])).to(self.device)
                 old_pos_x_log_prob = action_prob[:, step_count - 1, 1]
                 pos_x_ratio = torch.div(new_pos_x_prob, old_pos_x_log_prob).view((-1, 1))
                 pos_x_surr1 = pos_x_ratio * advantage
                 pos_x_surr2 = torch.clamp(pos_x_ratio, 1 - self.clip_param, 1 + self.clip_param) * advantage
 
-                new_pos_y_prob = torch.from_numpy(np.array(action['pos_y_prob']))
+                new_pos_y_prob = torch.from_numpy(np.array(action['pos_y_prob'])).to(self.device)
                 old_pos_y_log_prob = action_prob[:, step_count - 1, 2]
                 pos_y_ratio = torch.div(new_pos_y_prob, old_pos_y_log_prob).view((-1, 1))
                 pos_y_surr1 = pos_y_ratio * advantage
@@ -134,49 +153,18 @@ class PPO(object):
                 # self.writer.add_scalar('loss/value_loss', value_loss, global_step=self.training_step)
                 all_loss.backward()
 
-                self.optimizer.step()
-                print(all_loss.item())
+                total_loss += all_loss.item()
+                total_value += value_loss.item()
+                iter += 1
 
-        # state = torch.tensor([t.state for t in self.buffer], dtype=torch.float)
-        # action = torch.tensor([t.action for t in self.buffer], dtype=torch.long).view(-1, 1)
-        # reward = [t.reward for t in self.buffer]
-        # old_action_log_prob = torch.tensor([t.a_log_prob for t in self.buffer], dtype=torch.float).view(-1, 1)
-        #
-        # R = 0
-        # Gt = []
-        # for r in reward[::-1]:
-        #     R = r + self.gamma * R
-        #     Gt.insert(0, R)
-        # Gt = torch.tensor(Gt, dtype=torch.float)
-        # print("The agent is updateing....")
-        # for i in range(self.ppo_update_time):
-        #     for index in BatchSampler(SubsetRandomSampler(range(len(self.buffer))), self.batch_size, False):
-        #         if self.training_step % 1000 == 0:
-        #             print('I_ep {} ，train {} times'.format(i_ep, self.training_step))
-        #         # with torch.no_grad():
-        #         Gt_index = Gt[index].view(-1, 1)
-        #         V = self.ppo_net(state[index])
-        #         delta = Gt_index - V
-        #         advantage = delta.detach()
-        #         # epoch iteration, PPO core!!!
-        #         action_prob = self.ppo_net(state[index]).gather(1, action[index])  # new policy
-        #
-        #         ratio = (action_prob / old_action_log_prob[index])
-        #         surr1 = ratio * advantage
-        #         surr2 = torch.clamp(ratio, 1 - self.clip_param, 1 + self.clip_param) * advantage
-        #
-        #         # update actor critic network
-        #         action_loss = -torch.min(surr1, surr2).mean()  # MAX->MIN desent
-        #         self.optimizer.zero_grad()
-        #         action_loss.backward()
-        #         value_loss = F.mse_loss(Gt_index, V)
-        #         all_loss = action_loss + value_loss
-        #
-        #         nn.utils.clip_grad_norm_(self.ppo_net.parameters(), self.max_grad_norm)
-        #
-        #         # self.writer.add_scalar('loss/action_loss', action_loss, global_step=self.training_step)
-        #         # self.writer.add_scalar('loss/value_loss', value_loss, global_step=self.training_step)
-        #         all_loss.backward()
-        #
-        #         self.optimizer.step()
-        #         self.training_step += 1
+                self.optimizer.step()
+                print("epoch {}:{} all loss:{} value_loss:{} mean:{} mean value:{} ratio：{}".format(epoch, i,
+                                                                                                    all_loss.item(),
+                                                                                                    value_loss.item(),
+                                                                                                    total_loss / iter,
+                                                                                                    total_value / iter,
+                                                                                                    ratio.mean().item()))
+            if epoch % 100 == 0:
+                self.save()
+
+        self.save()
